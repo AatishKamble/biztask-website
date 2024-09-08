aa79732cec192a791600ae56d22ba746bd9df23f
AatishKamble <143265782+AatishKamble@users.noreply.github.com>
2024-09-09 00:45:21 +0530

    backend deployment connection

--- biztask-frontend/src/configApi/ConfigApi.js
+++ biztask-frontend/src/configApi/ConfigApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const API_BASE_URL="http://localhost:3000";
+export const API_BASE_URL="https://biztask.onrender.com";
 
 const jwt=localStorage.getItem("jwt");
 
