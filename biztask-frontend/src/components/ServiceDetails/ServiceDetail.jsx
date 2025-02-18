ed00097a3ca6aa0e6edf286d74193edf4910f4fc
AatishKamble <143265782+AatishKamble@users.noreply.github.com>
2025-02-18 18:36:08 +0530

    service page update

--- biztask-frontend/src/components/ServiceDetails/ServiceDetail.jsx
+++ biztask-frontend/src/components/ServiceDetails/ServiceDetail.jsx
@@ -48,6 +48,7 @@ const ServiceDetail = ({ serviceDetails, userDetails }) => {
         // new Chnage
         if (files.length === 0) {
             toast.error('Please upload at least one image.');
+            navigate(`/service-detail/${serviceDetails?._id}`)
             return;}
         for (let file of files) {
             formData.append("previousImages", file);
@@ -56,7 +57,6 @@ const ServiceDetail = ({ serviceDetails, userDetails }) => {
         formData.append("serviceId", serviceDetails._id);
         dispatch(uploadImage(formData, jwt));
         setFiles([]);
-      
         navigate(`/service-detail/${serviceDetails?._id}`)
     }
 
@@ -485,7 +485,7 @@ const ServiceDetail = ({ serviceDetails, userDetails }) => {
 
 
 
-                    {   !isLoading &&
+                    {
                         AllPhotos.length == 0 ?
                             <div className="w-full  flex items-center justify-center">
                                 <span className="text-[35px]  text-[#b0d0d2]  font-bold">
