d5e45944de6f2e4bb57a3ccf85037b509ee5a1f2
AatishKamble <143265782+AatishKamble@users.noreply.github.com>
2025-02-26 00:42:14 +0530

    Applied job ui

--- biztask-frontend/src/components/JobTemplate/JobDetail.jsx
+++ biztask-frontend/src/components/JobTemplate/JobDetail.jsx
@@ -346,13 +346,13 @@ const JobDetail = ({ userDetails, handleLogInButtonClick }) => {
     {/* Min Salary */}
     <div className="text-[20px] text-[#2E3A46] flex font-serif px-4 mb-2">
       <span className="font-semibold">Min Salary:</span>
-      <span className="pl-2 flex font-medium text-[#3B6064]"><MdOutlineCurrencyRupee className=" text-blue-700  " />{jobStore?.job?.minSalary}</span>
+      <span className="pl-2 flex font-medium items-center text-[#3B6064]"><MdOutlineCurrencyRupee className=" text-blue-700  " />{jobStore?.job?.minSalary}</span>
     </div>
 
     {/* Max Salary */}
     <div className="text-[20px] flex text-[#2E3A46] font-serif px-4">
       <span className="font-semibold">Max Salary:</span>
-      <span className="pl-2 font-medium flex text-[#3B6064]"><MdOutlineCurrencyRupee className=" text-blue-700  " />{jobStore?.job?.maxSalary}</span>
+      <span className="pl-2 font-medium flex items-center text-[#3B6064]"><MdOutlineCurrencyRupee className=" text-blue-700  " />{jobStore?.job?.maxSalary}</span>
     </div>
 
   </div>
